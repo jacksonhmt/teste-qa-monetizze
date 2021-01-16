f06520be420b6d3ab0370ddabd1d3e671b9c2951
Jackson Henrique <Jackson>
2021-01-16 15:42:57 -0300

    INSERT - INSERINDO PROJETO DE AUTOMACAO (JAVA/CUCUMBER)

---
+++ BDD/automacao_monetizze/src/main/java/br/com/jackson/monetizze/AutomacaoMonetizzeApplication.java
@@ -0,0 +1,13 @@
+package br.com.jackson.monetizze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomacaoMonetizzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomacaoMonetizzeApplication.class, args);
+	}
+
+}
---
+++ BDD/automacao_monetizze/src/test/java/br/com/jackson/monetizze/AutomacaoMonetizzeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jackson.monetizze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomacaoMonetizzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
